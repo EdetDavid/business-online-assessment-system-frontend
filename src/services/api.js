32b3724ad0644e20db1c5bff6b8e54b4e0887128
Dvooskid <dvooskid1234@gmail.com>
2025-05-06 12:08:33 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "https://business-assessment-system-backend.onrender.com/api/",
   withCredentials: true, // For CSRF token handling
 });
 
